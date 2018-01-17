ce6034e17b45eeeca0819333a917a3806124d19e
Semyon Fomin <taxnuke@gmail.com>
2018-01-17 03:37:50 +0300

    вносит автонумерацию и сохранение порядка билетов для выгрузки в Trello

--- trello.js
+++ trello.js
@@ -1,52 +1,75 @@
 #!/usr/bin/env node
 
+'use strict';
+
 const
   TRELLO_TOKEN = argFinder('--token'),
   TRELLO_KEY = argFinder('--key'),
   TRELLO_LIST = argFinder('--list'),
-  fileWithTickets = process.argv[2];
+  FILE_WITH_TICKETS = process.argv[2];
 
 const
   https = require('https'),
   readLine = require('readline'),
   fs = require('fs');
 
-readLine.createInterface({
-  input: fs.createReadStream(fileWithTickets)
-}).on('line', function(ticket) {
-  addNewTicketToTrello(ticket);
-});
+let
+  tickets = [],
+  currentTicketNumber = 1;
+
+readLine.createInterface({input: fs.createReadStream(FILE_WITH_TICKETS)})
+  .on('line', (ticket) => tickets.push(ticket))
+  .on('close', () => {
+    postNextTicketToTrello();
+  });
+
+function postNextTicketToTrello() {
+  const nextTicket = tickets.shift();
+
+  if (nextTicket) {
+    addNewTicketToTrello(nextTicket, postNextTicketToTrello);
+    ++currentTicketNumber;
+  } else {
+    console.log('Все билеты добавлены в Trello!');
+  }
+}
 
 /**
- * @param {string} subject Тема
- * @param {string} details Описание
+ * @param {string}   subject  Тема
+ * @param {string}   details  Описание
+ * @param {function} callback Коллбэк, вызываемый после добавления билета
  */
-function addNewTicketToTrello(subject, details) {
+function addNewTicketToTrello(subject, callback) {
+  const cardName = `${currentTicketNumber}. ` + subject;
+
   addCard(
-    subject,
-    details,
+    cardName,
+    null,
     TRELLO_LIST,
     null,
-    'top'
+    'bottom',
+    callback
   );
 }
 
 /**
- * @param {string} name     Название карточки
- * @param {string} desc     Описание
- * @param {string} idList   ID списка
- * @param {string} idLabels ID тегов
- * @param {string} pos      Положение в списке ('top' или 'bottom')
+ * @param {string}   name     Название карточки
+ * @param {string}   desc     Описание
+ * @param {string}   idList   ID списка
+ * @param {string}   idLabels ID тегов
+ * @param {string}   pos      Положение в списке ('top' или 'bottom')
+ * @param {function} callback Коллбэк, вызываемый после добавления карточки
  */
-function addCard(name, desc, idList, idLabels, pos) {
-  sendRequest('/cards', { name, desc, idList, idLabels, pos });
+function addCard(name, desc, idList, idLabels, pos, callback) {
+  sendRequest('/cards', { name, desc, idList, idLabels, pos }, callback);
 }
 
 /**
- * @param {string} route Маршрут API
- * @param {object} data  Данные в теле запроса
+ * @param {string}   route    Маршрут API
+ * @param {object}   data     Данные в теле запроса
+ * @param {function} callback Коллбэк, вызываемый после отправки запроса
  */
-function sendRequest(route, data) {
+function sendRequest(route, data, callback) {
   const
     BASE_URL = 'api.trello.com',
     API_V = 1;
@@ -65,11 +88,17 @@ function sendRequest(route, data) {
           console.log('Status: ' + res.statusCode);
           console.log('Body: ' + chunk);
           console.log('Data: ' + JSON.stringify(data));
+
+          process.exit(1);
+        } else {
+          callback();
         }
       });
     })
     .on('error', (e) => {
       console.log('Что-то пошло очень не так: ' + e.message);
+
+      process.exit(1);
     });
 
   req.setHeader('Content-Type', 'application/json');
